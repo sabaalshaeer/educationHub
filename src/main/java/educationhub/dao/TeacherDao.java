65842af25a32950b33ce248e607a2cde436d72d5
Saba Alshaeer <sabaalshaeer@gmail.com>
2023-08-18 02:12:37 -0500

    handle exceptions bycreate GlobalErrorHandler

--- src/main/java/educationhub/dao/TeacherDao.java
+++ src/main/java/educationhub/dao/TeacherDao.java
@@ -1,9 +1,13 @@
 package educationhub.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import educationhub.entity.Teacher;
 
 public interface TeacherDao extends JpaRepository<Teacher, Long> {
+
+	Optional<Teacher> findTeacherByEmail(String email);
 
 }
--- src/main/java/educationhub/service/TeacherService.java
+++ src/main/java/educationhub/service/TeacherService.java
@@ -3,8 +3,10 @@ package educationhub.service;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,27 +17,27 @@ import educationhub.model.TeacherData;
 
 @Service
 public class TeacherService {
-	
+
 	@Autowired
 	private TeacherDao teacherDao;
-	
+
 	@Autowired
-	private SchoolService schoolService;//inject this service because I need to call methods in this service
-	
+	private SchoolService schoolService;// inject this service because I need to call methods in this service
+
 	@Transactional(readOnly = false)
 	public TeacherData saveTeacher(Long schoolId, TeacherData teacherData) {
-		//fetch school by calling findSchoolById method which is in SchoolService class
+		// fetch school by calling findSchoolById method which is in SchoolService class
 		School school = schoolService.findSchoolById(schoolId);
-		//fetch the teacher id
+		// fetch the teacher id
 		Long teacherId = teacherData.getTeacherId();
-		//get teacher associate with the school
-		Teacher teacher = findOrCreateTeacher(schoolId, teacherId );
-		//set fields from teacherData to the teacher obj
+		// get teacher associate with the school
+		Teacher teacher = findOrCreateTeacher(schoolId, teacherId, teacherData);
+		// set fields from teacherData to the teacher obj
 		setTeacherFields(teacher, teacherData);
-		
-		teacher.setSchool(school);//set school to teacher
-		school.getTeachers().add(teacher);//add the teacher to the list of teacher that associate with the school
-		
+
+		teacher.setSchool(school);// set school to teacher
+		school.getTeachers().add(teacher);// add the teacher to the list of teacher that associate with the school
+
 		return new TeacherData(teacherDao.save(teacher));
 	}
 
@@ -45,37 +47,56 @@ public class TeacherService {
 		teacher.setTeacher_last_name(teacherData.getTeacher_last_name());
 		teacher.setEmail(teacherData.getEmail());
 		teacher.setTeacher_subject(teacherData.getTeacher_subject());
-		
+
 	}
 
-	private Teacher findOrCreateTeacher(Long schoolId, Long teacherId) {
-		if(teacherId == null) {
-			return new Teacher();
-		}else {
-			return findTeacherById(schoolId,teacherId);
+	private Teacher findOrCreateTeacher(Long schoolId, Long teacherId, TeacherData teacherData) {
+		// Checking if teacherId is null
+		if (teacherId == null) {
+			// Getting the email from teacherData
+			String email = teacherData.getEmail();
+
+			// Find existing teacher by email by passing variable email which we get it from teacherData
+			Optional<Teacher> existingTeacher = teacherDao.findTeacherByEmail(email);
+			// Check if an existing teacher with the same email exists
+			if (existingTeacher.isPresent()) {
+				throw new DuplicateKeyException("teacher with Email " + email + "exists");
+
+			} else {
+				// If no matching teacher was found, create a new teacher
+				Teacher newTeacher = new Teacher();
+				newTeacher.setEmail(email);// set the email
+				return newTeacher;
+			}
+		} else {
+			// If teacherId is provided, find the teacher by ID
+			return findTeacherById(schoolId, teacherId);
+
 		}
 	}
 
 	public Teacher findTeacherById(Long schoolId, Long teacherId) {
-		Teacher teacher = teacherDao.findById(teacherId).orElseThrow(() -> new NoSuchElementException("Teacher with id "+teacherId+ "not found"));
-	
-		//check if the teacher associate with the schoolId
-		if(teacher.getSchool().getSchoolId().equals(schoolId)){
+		Teacher teacher = teacherDao.findById(teacherId)
+				.orElseThrow(() -> new NoSuchElementException("Teacher with id " + teacherId + "not found"));
+
+		// check if the teacher associate with the schoolId
+		if (teacher.getSchool().getSchoolId().equals(schoolId)) {
 			return teacher;
-		}else {
-			throw new IllegalArgumentException("Teacher with Id "+teacherId+"is not associate with the school with id"+schoolId);
+		} else {
+			throw new IllegalArgumentException(
+					"Teacher with Id " + teacherId + "is not associate with the school with id" + schoolId);
 		}
 	}
 
 	@Transactional(readOnly = true)
 	public List<TeacherData> getAllTeachersAssWithSchoolWithId(Long schoolId) {
-		//fetch school by calling findSchoolById method which is in SchoolService class
+		// fetch school by calling findSchoolById method which is in SchoolService class
 		School school = schoolService.findSchoolById(schoolId);
-		//create list
+		// create list
 		List<TeacherData> listOfTeachersData = new LinkedList<>();
-		for(Teacher teacher : school.getTeachers()) {
+		for (Teacher teacher : school.getTeachers()) {
 			TeacherData teacherData = new TeacherData(teacher);
-			teacherData.getStudents().clear();//Don't get students which associate with the teacher
+			teacherData.getStudents().clear();// Don't get students which associate with the teacher
 			listOfTeachersData.add(teacherData);
 		}
 		return listOfTeachersData;
@@ -83,7 +104,8 @@ public class TeacherService {
 
 	@Transactional(readOnly = true)
 	public TeacherData getTeacherById(Long teacherId) {
-		Teacher teacher = teacherDao.findById(teacherId).orElseThrow(() -> new NoSuchElementException("Teacher with id "+teacherId+ "not found"));
+		Teacher teacher = teacherDao.findById(teacherId)
+				.orElseThrow(() -> new NoSuchElementException("Teacher with id " + teacherId + "not found"));
 		return new TeacherData(teacher);
 	}
 
