d219d5df841db0df13e0e6533578bc8f3bdba37b
Saba Alshaeer <sabaalshaeer@gmail.com>
2023-08-30 01:27:25 -0500

    uncomment data.sql file and change application.yaml sql init mode to never so it des not effect when I run java application

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,10 +1,12 @@
-INSERT INTO student (student_first_name, student_last_name, age, student_email, grade)
-VALUES('Rana', 'alshaeer', 15, 'rana@icon.com', 10);
-INSERT INTO student (student_first_name, student_last_name, age, student_email, grade)
-VALUES('Laith', 'Bachachi', 15, 'laith@icon.com', 10);
-INSERT INTO student (student_first_name, student_last_name, age, student_email, grade)
-VALUES('Hasan', 'Juaifari', 16, 'hasanfari@icon.com', 11);
-INSERT INTO student (student_first_name, student_last_name, age, student_email, grade)
-VALUES('Hayder', 'Juaifari', 16, 'hjuaifari@icon.com', 11);
+--insert students , uncommet these insert statement for test only
+
+--INSERT INTO student (student_first_name, student_last_name, age, student_email, grade)
+--VALUES('Rana', 'alshaeer', 15, 'rana@icon.com', 10);
+--INSERT INTO student (student_first_name, student_last_name, age, student_email, grade)
+--VALUES('Laith', 'Bachachi', 15, 'laith@icon.com', 10);
+--INSERT INTO student (student_first_name, student_last_name, age, student_email, grade)
+--VALUES('Hasan', 'Juaifari', 16, 'hasanfari@icon.com', 11);
+--INSERT INTO student (student_first_name, student_last_name, age, student_email, grade)
+--VALUES('Hayder', 'Juaifari', 16, 'hjuaifari@icon.com', 11);
   
  
